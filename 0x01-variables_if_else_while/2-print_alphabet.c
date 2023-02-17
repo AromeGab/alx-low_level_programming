f42da5bb0d447bbe8f8f6f3c90f45e630caf0f1b
AromeGab <aromegabrieljnr@gmail.com>
2023-02-17 09:46:49 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program to produce the alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; c++)
+	{
+		putchar(x);
+	}
+	puchar('\n');
+	return (0);
+}
