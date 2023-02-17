ea39c02c2a8fa39e00d8b24e72a36e494316283d
AromeGab <aromegabrieljnr@gmail.com>
2023-02-17 10:58:19 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - A Program to print hexadecimal numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 16; x++)
+	{
+		if (n < 10)
+		{
+			putchar('0' + x);
+		}
+		else
+		{
+			putchar(87 + x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
