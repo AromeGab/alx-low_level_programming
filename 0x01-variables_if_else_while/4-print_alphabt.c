de325fe0049b9813f144867146f0db27b4bd22cc
AromeGab <aromegabrieljnr@gmail.com>
2023-02-17 10:09:48 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print all aphabets except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' || x != 'e')
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
