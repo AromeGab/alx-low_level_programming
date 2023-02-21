18ab87c5314479980b42fa53dea7a0839676f070
AromeGab <aromegabrieljnr@gmail.com>
2023-02-21 13:53:23 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a program to print putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i];
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is now on Functions and Nested loops
