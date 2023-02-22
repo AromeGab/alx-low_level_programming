5f7bce9c90708479ca870a812d96638655aaa3c8
AromeGab <aromegabrieljnr@gmail.com>
2023-02-22 07:44:05 -0800

    modified task 9 solution

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,54 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0.
-**/
+ * times_table - prints the 9 times table
+ *
+ * Example Table
+ * 0, 0, 0, 0, ..
+ * 0, 1, 2, 3, ..
+ *
+ * Return: Always 0 (Success)
+*/
 
 void times_table(void)
 {
-	int i, j, k;
+	int a, b, result;
 
-	for (i = 0; i <= 9; i++)
+	/*first character*/
+	for (a = 0; a <= 9; a++)
 	{
-
-		for (j = 0; j <= 9; j++)
+		/*second character*/
+		for (b = 0; b <= 9; b++)
 		{
-			k = i * j;
-
-			if (k <= 9)
+			/*print 0 for the first column*/
+			if (b == 0)
 			{
-				_putchar(' ');
+				_putchar('0');
+				continue;
+			}
+
+			/*multiply a and b*/
+			result = a * b;
+			_putchar(',');
+			_putchar(' ');
+
+			/**
+			 * if result is more and equal to 10
+			 * we will be forced to get the first
+			 * digit and second digit of product
+			 * and print them out respectively
+			*/
+			if (result >= 10)
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
 			}
 			else
 			{
-				_putchar((k / 10) + '0');
-			}
-			_putchar((k % 10) + '0');
-
-			if (j != 9)
-			{
-				_putchar(',');
 				_putchar(' ');
+				_putchar(result + '0');
 			}
 		}
-
 		_putchar('\n');
 	}
 }
