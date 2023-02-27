7acedb823c2f5ebedb1b7013b0f820d35e6e84e4
AromeGab <aromegabrieljnr@gmail.com>
2023-02-27 04:58:56 -0800

    0x05. C - Pointers, arrays and strings AromeGab

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this is meant to  update the value it points to to 98.
+ * @n: this represents the paramater to be passed
+**/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Now on Pointers Arrays and Strings
